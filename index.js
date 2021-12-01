0ab1536057615d43e64debbd1d96d41c0fd850a9
pg-4919 <77947679+pg-4919@users.noreply.github.com>
2021-11-30 20:10:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 const discord = require("discord.js");
 const express = require("express");
 
-const client = new discord.Client({ intents: [ "GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS" ]});
+const client = new discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_VOICE_STATES", "GUILD_MEMBERS" ]});
 
 client.on("messageCreate", message => {
-    const author = message.author;
-    const guild = message.guild;
     const content = message.content;
+    const guild = message.guild;
 
-    if (author.id === "708040197228724296") {
+    if (message.author.id === process.env.annoyee) {
         if (content === "") content = "⠀";
-        return message.delete();
+        message.channel.send(message);
+        message.delete();
     }
 
-    guild.members.cache.get("913958285110050847")
-        .setNickname(guild.members.cache.get("708040197228724296").nickname);
+    guild.members.cache.get(client.user.id)
+        .setNickname(guild.members.cache.get(process.env.annoyee).nickname);
+    return;
 });
 
 client.on("guildMemberUpdate", (old, updated) => {
-    if (updated.user.id === "708040197228724296") {
+    if (updated.user.id === process.env.annoyee) {
         const guild = client.guilds.cache.get(updated.guild.id);
-        const self = guild.members.cache.get("913958285110050847");
+        const self = guild.members.cache.get(client.user.id);
         self.setNickname(updated.nickname);
     }
     return;
@@ -30,11 +31,6 @@ client.on("ready", () => {
     console.log("Ready to begin annoying the heck outta people");
     client.user.setStatus("invisible");
 })
-
-/* client.on("userUpdate", (old, updated) => {
-    console.log(updated.displayAvatarURL())
-    client.user.setAvatar(updated.displayAvatarURL());
-}); */
 
 client.login(process.env.token);
 
